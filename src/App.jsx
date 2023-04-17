ae87ae2514169727be7b0d7793e2acde8aa9e8ce
Saqib Bhatti <66674431+SQZ0111@users.noreply.github.com>
2023-04-17 16:33:26 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Headline from './components/Headers/Headline'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Headline text="Hallo Welt"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Headers/Headline.jsx
@@ -0,0 +1,12 @@
+export default function Headline({text}) {
+    return(
+    <>
+        <h1 style={{
+            textAlign:"center",
+            color: "green",
+            fontFamily: "cursive",
+            fontSize: "36px"
+        }}>{text}</h1>
+    </>
+    );
+}
