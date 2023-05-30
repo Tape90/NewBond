6da6faf202bf0b2f6184d6b49d293f7f9fad9b3a
Saqib Bhatti <saqibbhatti.public@gmail.com>
2023-05-30 11:14:32 +0200

    map renders properly

--- frontend/src/components/Modals/MapModal.jsx
+++ frontend/src/components/Modals/MapModal.jsx
@@ -1,20 +1,33 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Modal } from "@mui/material";
+import 'leaflet/dist/leaflet.css';
 
 
 export default function MapModal({closeMapModal,posts,showMapModal}) {
     return(
         <>
-        <Modal 
+        <Modal
+        sx={{
+          width: "80vw",
+          height: "80vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
         open={showMapModal}
         onClose={closeMapModal}>
-          <MapContainer center={[51.1657, 10.4515]} zoom={6} style={{ height: '400px', width: '100%' }}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <MapContainer center={[51.4792732, 7.2032233]} zoom={10} style={{ height: '100%', width: '100%'}} scrollWheelZoom={false}>
+            <TileLayer 
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            iconUrl="../../../public/icons/marker-icon.png"
+            iconRetinaUrl="../../../public/icons/marker-icon-2x.png"
+            shadowUrl="../../../public/icons/marker-shadow.png" />
             {posts.map((post) => (
+              
               <Marker key={post.id} position={[post.latitude, post.longitude]}>
                 <Popup>
                   <h3>{post.title}</h3>
-                  <p>{post.location}</p>
+                  <p>{post.place}</p>
                 </Popup>
               </Marker>
             ))}
